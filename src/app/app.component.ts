31a692efba09cd7be7ec346327dcc7f8be28a45b
VasylPasichnyk <pasichnykvv@gmail.com>
2022-07-07 17:42:20 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+type Currency = {
+  ccy: string;
+  base_ccy: string;
+  buy: number;
+  sale: number;
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'converter';
+export class AppComponent implements OnInit {
+  public exchangeForm: FormGroup;
+
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
+    this.exchangeForm = this.formBuilder.group({
+      fromInput: 0,
+      toInput: 0,
+    });
+  }
+
+  currencies: Array<Currency> = [
+    { ccy: 'UAH', base_ccy: 'UAH', buy: 1, sale: 1 },
+  ];
+  from = {
+    currency: 'UAH',
+  };
+  to = {
+    currency: 'UAH',
+  };
+
+  renderHeader = () => {
+    return this.currencies.filter(
+      (item) => item.ccy === 'USD' || item.ccy === 'EUR'
+    );
+  };
+
+  kross = () => {
+    let fromAmount = this.currencies.find(
+      (item) => item.ccy === this.from.currency
+    );
+    let toAmount = this.currencies.find(
+      (item) => item.ccy === this.to.currency
+    );
+    let result = fromAmount!.buy / toAmount!.buy;
+    return result;
+  };
+
+  ngOnInit() {
+    this.http
+      .get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
+      .subscribe((data: any) => {
+        console.log(data)
+        data.forEach((element:any) => {
+          if(element.base_ccy !== 'UAH') {
+            let base = data.find((el: any) => el.ccy === element.base_ccy)
+            element.buy = (element.buy*base.buy).toString()
+            element.sale = (element.sale*base.sale).toString()
+            element.base_ccy = base.base_ccy
+          }
+        });
+
+        this.currencies.push(...data);
+        console.log(this.currencies)
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+  BrowserModule,
+  HttpClientModule,
+  FormsModule,
+  BrowserAnimationsModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
